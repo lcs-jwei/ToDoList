fe5c07e714804de75fb46f8224902e3435eb50d7
Justin <jwei@lcs.on.ca>
2023-04-03 14:46:52 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Justin Zack Wei on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
